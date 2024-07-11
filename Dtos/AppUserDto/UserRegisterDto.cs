b9a8c21b1718f6dd5fc22a0ac868648f4c979648
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-07-11 11:29:42 +0300

    Add user registration functionality
    This commit introduces a new user registration feature to the HotelProject.WebUI project. A new C# class, UserRegisterDto, has been added under the Dtos/AppUserDto namespace, encapsulating properties necessary for user registration such as Name, Surname, City, Mail, Password, and ConfirmPassword. These properties are equipped with data annotations for validation, including [Required], [StringLength], [EmailAddress], and [Compare], each with custom error messages to improve user feedback.
    
    Additionally, a new Razor view, Register.cshtml, has been created to serve as the registration page. This view is strongly typed with the UserRegisterDto class to ensure proper data binding. It is designed as a standalone page with its layout set to null and includes a user-friendly registration form with validation feedback, styled with external resources like Font Awesome, custom CSS, and JavaScript files. The form is configured to post data to the Register action method of the User controller. Links to the login page and various scripts for enhanced interactivity and visual appeal, including a preloader animation, are also integrated.

---
+++ Dtos/AppUserDto/UserRegisterDto.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.AppUserDto
+{
+    public class UserRegisterDto
+    {
+        [Required(ErrorMessage = "Lütfen kullanıcı ismini giriniz.")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Lütfen kullanıcının soy ismini giriniz.")]
+        public string? Surname { get; set; }
+        [Required(ErrorMessage = "Lütfen şehir bilgisini giriniz.")]
+        public string? City { get; set; }
+        [Required(ErrorMessage = "Lütfen mail giriniz.")]
+        [StringLength(50)]
+        [EmailAddress]
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifrenizi giriniz.")]
+        [StringLength(50)]
+        [MinLength(5, ErrorMessage = "Şifre 5 karakterden kısa olamaz!")]
+        [MaxLength(50, ErrorMessage = "Şifre 50 karakterden uzun olamaz!")]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Şifreler uyuşmuyor.")]
+        [Required(ErrorMessage = "Lütfen şifrenizi tekrar giriniz.")]
+        [StringLength(50)]
+        [MinLength(5, ErrorMessage = "Şifre 5 karakterden kısa olamaz!")]
+        [MaxLength(50, ErrorMessage = "Şifre 50 karakterden uzun olamaz!")]
+        public string ConfirmPassword { get; set; }
+    }
+}
