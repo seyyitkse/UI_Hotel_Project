d01d7baf46cb0c77762b97b65977b87624d11820
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-07-16 11:16:57 +0300

    Add HomePageController with MVC setup
    Added a new `using` directive for `Microsoft.AspNetCore.Mvc` to utilize MVC framework features in `HomePageController.cs`. Introduced a namespace `HotelProject.WebUI.Controllers` for organizing the new `HomePageController` class, which inherits from the `Controller` base class to handle web requests. Implemented a `HomePage()` action method to return views, facilitating the display of the application's home page.

---
+++ Controllers/HomePageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class HomePageController : Controller
+    {
+        public IActionResult HomePage()
+        {
+            return View();
+        }
+    }
+}
