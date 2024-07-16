e9c4a60ea21291b543825745dde44a6f06892c06
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-07-16 11:18:11 +0300

    Add AuthController and related views
    - Introduced AuthController with actions for login and registration.
    - Added UserLoginDto for handling login data with validation.
    - Created Login.cshtml and modified Register.cshtml for user authentication.
    - Implemented HTTP POST requests in AuthController actions for interacting with an external API.
    - Enhanced user interface and navigation between login and registration pages.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,75 @@
+﻿using HotelProject.WebUI.Dtos.AppUserDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AuthController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        // Kullanıcı giriş işlemi
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(UserLoginDto model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:5077/api/Auth/login", stringContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError(string.Empty, "Giriş başarısız!");
+            return View(model);
+        }
+
+        // Kullanıcı kayıt işlemi
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(UserRegisterDto model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:5077/api/Auth/register", stringContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError(string.Empty, "Kayıt başarısız!");
+            return View(model);
+        }
+    }
+}
---
+++ Dtos/AppUserDto/UserLoginDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.AppUserDto
+{
+    public class UserLoginDto
+    {
+        [Required(ErrorMessage = "Mail alanı boş geçilemez!")]
+        [StringLength(50, ErrorMessage = "Mail alanı en fazla 50 karakter olabilir!")]
+        [MinLengthAttribute(5, ErrorMessage = "Mail alanı en az 5 karakter olabilir!")]
+        public string Mail { get; set; }
+        [Required(ErrorMessage = "Şifre alanı boş geçilemez!")]
+        [StringLength(50, ErrorMessage = "Şifre alanı en fazla 50 karakter olabilir!")]
+        [MinLengthAttribute(5, ErrorMessage = "Şifre alanı en az 5 karakter olabilir!")]
+        public string Password { get; set; }
+    }
+}
