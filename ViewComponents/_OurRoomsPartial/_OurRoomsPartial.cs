9049edd0ff193f0f8e7a9a4cb0d50a7e37917325
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-08-05 18:50:20 +0300

    Refactor _OurRoomsPartial to use async HTTP requests
    Updated using directives to include HotelProject.WebUI.Dtos.AboutDto and Newtonsoft.Json.
    Injected IHttpClientFactory into _OurRoomsPartial.
    Added constructor to initialize IHttpClientFactory.
    Changed Invoke method to async for HTTP GET request.
    Deserialized JSON response into ResultAboutDto list.
    Handled HTTP request failure by returning view without data.

--- ViewComponents/_OurRoomsPartial/_OurRoomsPartial.cs
+++ ViewComponents/_OurRoomsPartial/_OurRoomsPartial.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.AboutDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WebUI.ViewComponents._OurRoomsPartial
 {
-    public class _OurRoomsPartial: ViewComponent
+    public class _OurRoomsPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _OurRoomsPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> Invoke()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5077/api/Room");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
