b0f579debb5aa6849dc14b0913b47b438583c771
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-08-09 12:01:30 +0300

    Refactor partials and Default.cshtml for async data fetching
    Refactored `_ServicePartial.cs` and `_TestimonialPartial.cs` to use
    dependency injection for `IHttpClientFactory` and fetch data
    asynchronously from APIs. Updated `Default.cshtml` to dynamically
    generate service and testimonial items based on the model data,
    replacing hardcoded content.

--- ViewComponents/_TestimonialPartial/_TestimonialPartial.cs
+++ ViewComponents/_TestimonialPartial/_TestimonialPartial.cs
@@ -1,10 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Models.Testimonial;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 namespace HotelProject.WebUI.ViewComponents._TestimonialPartial
 {
     public class _TestimonialPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TestimonialPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5077/api/Testimonial");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<TestimonialViewModel>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
