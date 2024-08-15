dd84d5dd63a05e7c2aed5d066d970c312fd4ee41
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-08-15 09:56:58 +0300

    Add newsletter subscription feature and UI updates
    - Inject IHttpClientFactory into DefaultController
    - Add GET and POST methods for _NewsletterPartial in DefaultController
    - Remove _NewsletterPartial ViewComponent class
    - Use Html.PartialAsync in Index.cshtml for _NewsletterPartial
    - Update Default.cshtml with new section and translations
    - Add CreateSubscribeDto for email validation
    - Add form and client-side validation in _NewsletterPartial.cshtml

--- Controllers/DefaultController.cs
+++ Controllers/DefaultController.cs
@@ -1,12 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.SubscribeDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public PartialViewResult _NewsletterPartial()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> _NewsletterPartial(string email)
+        {
+            var createSubscribeDto = new CreateSubscribeDto
+            {
+                Mail = email
+            };
+
+            if (string.IsNullOrEmpty(email))
+            {
+                ModelState.AddModelError("Mail", "Email adresi giriniz!");
+                return PartialView();
+            }
+
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createSubscribeDto);
+            StringContent jsonService = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:5077/api/Subscribe", jsonService);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return PartialView();
         }
     }
 }
---
+++ Dtos/SubscribeDto/CreateSubscribeDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.SubscribeDto
+{
+    public class CreateSubscribeDto
+    {
+        [Required(ErrorMessage = "Email adresi giriniz!")]
+        public string? Mail { get; set; }
+    }
+}
