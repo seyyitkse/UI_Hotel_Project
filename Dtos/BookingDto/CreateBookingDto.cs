6f75b2db63856af3b1e25637c99f478e22f9d941
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-08-26 23:28:50 +0300

    Add CreateBookingDto and update BookingController
    - Updated BookingController.cs to include dependency injection for IHttpClientFactory and use CreateBookingDto for booking creation.
    - Modified AddBooking.cshtml to use CreateBookingDto model, updated form fields, labels, and placeholders to Turkish.
    - Added date picker scripts to AddBooking.cshtml for check-in and check-out dates.
    - Created CreateBookingDto.cs defining the CreateBookingDto class with properties for booking details.

---
+++ Dtos/BookingDto/CreateBookingDto.cs
@@ -0,0 +1,19 @@
+﻿namespace HotelProject.WebUI.Dtos.BookingDto
+ {
+    public class CreateBookingDto
+    {
+        public string Name { get; set; }
+        public string Mail { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public int AdultCount { get; set; }
+        public int ChildCount { get; set; }
+        public int RoomCount { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public string SpecialRequests { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+    }
+}
