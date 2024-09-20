ad2dc486d9fb52b08e28c22f20bfc6ce7a132dd5
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-09-20 19:31:48 +0300

    Add new actions and imports to BookingAdminController
    - Added import for `ServiceDto` from `HotelProject.WebUI.Dtos`.
    - Added import for `System.Text`.
    - Introduced `Details(int id)` action to fetch booking details.
    - Introduced `Delete(int id)` action to delete a booking.
    - Introduced `ApprovedReservation(ApprovedReservationDto)` action to approve a reservation.

--- Controllers/BookingAdminController.cs
+++ Controllers/BookingAdminController.cs
@@ -1,6 +1,8 @@
 ﻿using HotelProject.WebUI.Dtos.BookingDto;
+using HotelProject.WebUI.Dtos.ServiceDto;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace HotelProject.WebUI.Controllers
 {
@@ -21,6 +23,41 @@ namespace HotelProject.WebUI.Controllers
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<ResultBookingDto>>(jsonData);
                 return View(values);
+            }
+            return View();
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5077/api/Reserve/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultBookingDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync("http://localhost:5077/api/Reserve/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public async Task<IActionResult> ApprovedReservation(ApprovedReservationDto approvedReservationDto)
+        {
+            approvedReservationDto.Status = "Onaylandı";
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(approvedReservationDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "Application/Json");
+            var responseMessage = await client.PutAsync("http://localhost:5077/api/Reserve/BookingApprovedId", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
             }
             return View();
         }
