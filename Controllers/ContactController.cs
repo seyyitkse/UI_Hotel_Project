ee46eaca1d3eb4cce7a003ce9dda3c7ca11b2433
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-09-21 21:44:21 +0300

    Add ContactController and update Google Maps location
    Added a new ContactController class in ContactController.cs to handle HTTP requests for contact-related actions, including sending messages. Updated SendMessage.cshtml to change the Google Maps iframe source URL from New York to Ankara.

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,50 @@
+﻿using HotelProject.WebUI.Dtos.ContactDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SendMessage(CreateContactDto createContactDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+
+            // Optional sample logic
+            createContactDto.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createContactDto);
+            StringContent jsonService = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            var responseMessage = await client.PostAsync("http://localhost:5077/api/Contact", jsonService);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // You may log the error or provide feedback to the user
+                ModelState.AddModelError("", "Contact failed. Please try again.");
+                return View("Index","Default");
+            }
+        }
+    }
+}
