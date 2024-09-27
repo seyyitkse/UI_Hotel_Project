56149538735f8d4cd86b4618a304e0e0502efcb8
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-09-27 19:24:13 +0300

    Add GuestController and related DTOs and views
    Added a new `GuestController` class in `GuestController.cs` to handle guest-related operations, including listing, adding, deleting, and updating guests. The controller uses `IHttpClientFactory` for API requests.
    
    Introduced `CreateGuestDto`, `ResultGuestDto`, and `UpdateGuestDto` classes to represent data transfer objects for creating, displaying, and updating guest information.
    
    Added views `AddGuest.cshtml`, `Index.cshtml`, and `UpdateGuest.cshtml` to provide forms and tables for managing guests.

---
+++ Dtos/GuestDto/CreateGuestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelProject.WebUI.Dtos.GuestDto
+{
+    public class CreateGuestDto
+    {
+        public string GuestName { get; set; }
+        public string GuestSurname { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Dtos/GuestDto/ResultGuestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelProject.WebUI.Dtos.GuestDto
+{
+    public class ResultGuestDto
+    {
+        public int GuestId { get; set; }
+        public string GuestName { get; set; }
+        public string GuestSurname { get; set; }
+        public string City { get; set; }
+    }
+}
