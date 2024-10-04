ecae6a43372d50fc0f1d209d12100a36eb784ddf
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-10-04 14:34:24 +0300

    Add messaging functionality to AdminContactController
    - Added DTOs: CreateSendMessageDto and ResultSendMessageDto.
    - Enhanced AdminContactController with methods for message creation, sending, and viewing.
    - Updated Inbox.cshtml to link to message details and show more info.
    - Created new views: AddSendMessage, MessageDetailsInbox, MessageDetailsSendbox, and Sendbox.
    - Modified SidebarAdminContactPartialAsync.cshtml to update links and badge counts.

--- Controllers/AdminContactController.cs
+++ Controllers/AdminContactController.cs
@@ -1,6 +1,9 @@
 ﻿using HotelProject.WebUI.Dtos.ContactDto;
+using HotelProject.WebUI.Dtos.SendMessageDto;
+using HotelProject.WebUI.Models.Staff;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace HotelProject.WebUI.Controllers
 {
@@ -25,10 +28,68 @@ namespace HotelProject.WebUI.Controllers
             }
             return View();
         }
-        public PartialViewResult SidebarAdminContactPartial()
+        [HttpGet]
+        public IActionResult AddSendMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddSendMessage(CreateSendMessageDto model)
+        {
+            model.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            model.SenderMail = "admin@mail.com";
+            model.SenderName = "Admin";
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:5077/api/SendMessage", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Inbox");
+            }
+            return View();
+        }
+        public async Task<IActionResult> Sendbox()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5077/api/SendMessage");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultSendMessageDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        public async Task<IActionResult> MessageDetailsInbox(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:5077/api/SendMessage/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultContactDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        public async Task<IActionResult> MessageDetailsSendbox(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:5077/api/SendMessage/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultSendMessageDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        public PartialViewResult SidebarAdminContactPartialAsync()
         {
             return PartialView();
         }
+
         public PartialViewResult SidebarAdminContactCategoryPartial()
         {
             return PartialView();
